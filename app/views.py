58444ed235e6b68cab1885fde437b1c0a3315c44
Demarillac Izere <izered3@gmail.com>
2020-11-21 15:28:20 +0200

    added a news categories in the index view function

--- app/views.py
+++ app/views.py
@@ -8,4 +8,17 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    science_news = get_articles('science')
+    entertainment_news = get_movies('entertainment')
+    general_news = get_movies('general')
+    title = 'Home - Browse news articles from all around the world'
+
+    # search_movie = request.args.get('movie_query')
+
+    # if search_movie:
+    #     return redirect(url_for('search',movie_name=search_movie))
+    # else:
+    #     return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
+
+    return render_template('index.html', title = title, science = science_news, entertainment = entertainment_news, general = general_news )
