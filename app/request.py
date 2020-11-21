461b0254d9c5b3199bb6aae2997ea0787bd80550
Demarillac Izere <izered3@gmail.com>
2020-11-21 19:37:41 +0200

    added a function to create source object

--- app/request.py
+++ app/request.py
@@ -48,3 +48,30 @@ def process_results(article_list):
             article_results.append(article_object)
 
     return article_results
+
+def get_sources(sources):
+    get_source_url=source_url.format(sources,api_key)
+    with urllib.request.urlopen(get_source_url) as url:
+        source_data=url.read()
+        source_response=json.loads(source_data)
+
+        source_results=None
+
+        if source_response["sources"]:
+            new_source_results=source_response["sources"]
+            source_results=process_sources(new_source_results)
+    return source_results
+
+def process_sources(source_list):
+    source_results=[]
+    for source in source_list:
+        id=source.get("id")
+        name=source.get("name")
+        description=source.get("description")
+        url=source.get("url")
+
+        source_object=Sources(id,name,description,url)
+        source_results.append(source_object)
+        source_results=source_results[:4]
+
+    return source_results
