1c849fb1e8bc54ec676a33ebb381ad8b080c7e12
Demarillac Izere <izered3@gmail.com>
2020-11-21 16:06:19 +0200

    added an function to create articles objects

--- app/request.py
+++ app/request.py
@@ -3,5 +3,50 @@ import urllib.request,json
 from .models import Article, Sources
 
 Article = article.Article
-# Getting api key
 api_key = app.config['NEWS_API_KEY']
+base_url = app.config["NEWS_API_BASE_URL"]
+
+def get_articles(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_articles_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+
+        articles_results = None
+
+        if get_articles_response['results']:
+            article_results_list = get_articles_response['results']
+            article_results = process_results(article_results_list)
+
+
+    return article_results
+
+def process_results(article_list):
+    '''
+    Function  that processes the article result and transform them to a list of Objects
+
+    Args:
+        article_list: A list of dictionaries that contain article details
+
+    Returns :
+        article_results: A list of article objects
+    '''
+    article_results = []
+    for article_item in article_list:
+        author = article_item.get('author')
+        title = movie_item.get('original_title')
+        description = article_item.get('description')
+        urlToImage = article_item.get('urlToimage')
+        content = article_item.get('content')
+        url = article_item.get.('url')
+        publishedAt= article_item.get('publishedAt')
+
+        
+        article_object = Article(author,title,description,urlToImage,url,content,publishedAt)
+        article_results.append(article_object)
+
+    return movie_results
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from ..request import get_articles
 
 # Views
 @app.route('/')
@@ -10,8 +11,8 @@ def index():
     '''
 
     science_news = get_articles('science')
-    entertainment_news = get_movies('entertainment')
-    general_news = get_movies('general')
+    entertainment_news = get_articles('entertainment')
+    general_news = get_articles('general')
     title = 'Home - Browse news articles from all around the world'
 
     # search_movie = request.args.get('movie_query')
